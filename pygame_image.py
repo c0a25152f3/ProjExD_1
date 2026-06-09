0bddefdac8ab6770cc7de750ca91480a7fd0bdc2
ballet <c0a25152f3@edu.teu.ac.jp>
2026-06-09 17:01:22 +0900

    演習1

--- pygame_image.py
+++ pygame_image.py
@@ -21,6 +21,7 @@ def main():
             if event.type == pg.QUIT: return
 
         key_lst = pg.key.get_pressed()
+        kk_rct.move_ip(-1, 0)
 
         if key_lst[pg.K_UP]:  # 上矢印キーが押されたら
             kk_rct.move_ip(0, -1)  # 上に移動
@@ -29,7 +30,7 @@ def main():
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip(-1, 0)
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(+1, 0)
+            kk_rct.move_ip(+2, 0)
 
         x = tmr%3200
         screen.blit(bg_img, [-x, 0])
